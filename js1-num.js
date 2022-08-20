cdcbc8e2f267565b2b2dcfe3ad8ed31e08357130
Ekrem <ekremteke.uk@gmail.com>
2022-08-20 13:00:31 +0100

    percentages

--- js1-num.js
+++ js1-num.js
@@ -1,9 +1,26 @@
-const numberNumber = 1;
+// const numberNumber = 1;
 
-const sum = 10 + 2;
-const product = 10 * 5;
-const quotient = 10 / 2;
-const difference = 50 - 5;
+// const sum = 10 + 2;
+// const product = 10 * 5;
+// const quotient = 10 / 2;
+// const difference = 50 - 5;
 
-console.log(sum, product, quotient, difference);
+// console.log(sum, product, quotient, difference);
 
+// const ekremAge = 33.5454;
+
+// const roundedAge = Math.round(ekremAge);
+// console.log(roundedAge);
+
+const numberOfTrainees = 40;
+const numberOfTa = 7;
+const totalAttandence = numberOfTa + numberOfTrainees;
+
+const percentageOfTa = (numberOfTa * 100) / totalAttandence;
+console.log(percentageOfTa);
+
+const percentageOfTrainees = (numberOfTrainees * 100) / totalAttandence;
+
+console.log("Percentage of mentors: " + percentageOfTa);
+
+console.log("Percentage of trainees: " + percentageOfTrainees);
