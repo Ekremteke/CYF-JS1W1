dd0bd66d2dbc4342437891a6a5c968bce5bf0b40
Ekrem <ekremteke.uk@gmail.com>
2022-08-20 11:26:43 +0100

    wrote three times

--- js1.js
+++ js1.js
@@ -1,2 +1,9 @@
 console.log("Hello World");
 console.log("is It Work?");
+
+//variables
+
+const turkishGreating = 'Merhaba Dunya';
+console.log(turkishGreating);
+console.log(turkishGreating);
+console.log(turkishGreating);
