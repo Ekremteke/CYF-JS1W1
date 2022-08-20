21b6c5f1afceb7605b7eb74ec38280de9921c9b7
Ekrem <ekremteke.uk@gmail.com>
2022-08-20 16:03:31 +0100

    exercise 1- 2 and 3

--- Expression.js
+++ Expression.js
@@ -1,7 +1,36 @@
-"use strict";
+function formatPenceToPounds(a) {
 
+    return "£" + (a / 100);
+}
+let a = 155;
+console.log(formatPenceToPounds(a));
 
+function increaseByHalf(b) {
 
-test > 100;
+    return b * 1.5;
+}
+let b = 100;
 
-let test = console.log(90);
+console.log(increaseByHalf(b));
+
+let pocketMoney = 413;
+
+let myNewPence = increaseByHalf(pocketMoney);
+
+let myNewPocketMoney = formatPenceToPounds(myNewPence);
+
+console.log(myNewPocketMoney);
+
+function printMessage(x) {
+    console.log("Hello, nice to meet you " + x);
+}
+
+function getMessage(name) {
+    return "Hello, nice to meet you " + name;
+}
+
+printMessage("Mitch");
+
+printMessage("August");
+
+getMessage("Laetitia");
