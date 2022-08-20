e88485c60adcd95ccad6f6c8faf00476d6d562a6
Ekrem <ekremteke.uk@gmail.com>
2022-08-20 12:24:53 +0100

    new one-third one-fourth one

--- js1.js
+++ js1.js
@@ -1,13 +1,32 @@
-console.log("Hello World");
-console.log("is It Work?");
+// console.log("Hello World");
+// console.log("is It Work?");
 
-//variables
+// //variables
 
-const turkishGreating = 'Merhaba Dunya';
-console.log(turkishGreating);
-console.log(turkishGreating);
-console.log(turkishGreating);
+// const turkishGreating = 'Merhaba Dunya';
+// console.log(turkishGreating);
+// console.log(turkishGreating);
+// console.log(turkishGreating);
 
-var greeting = "hello";
-greeting = "merhaba";
+// var greeting = "hello";
+// greeting = "merhaba";
+// console.log(greeting);
+
+const firstName = "Ekrem";
+const space = " ";
+const firstString = "My name is";
+
+const greeting = firstString + space + firstName;
 console.log(greeting);
+
+//interpolation
+const secondString = "new one";
+
+const thirdString = "third one";
+
+const fourthString = "fourth one"
+
+const anyThing = `${secondString}-${thirdString}-${fourthString}`;
+//new one-third one-fourth one
+
+console.log(anyThing);
