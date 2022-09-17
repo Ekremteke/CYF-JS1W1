c95f3018e51ec6e2b366c217b354656e7080544d
Ekrem <ekremteke.uk@gmail.com>
2022-09-17 12:28:37 +0100

    fifference between null (error) and undefined

--- JS1W3exercises/exercise1.js
+++ JS1W3exercises/exercise1.js
@@ -1,3 +1,7 @@
 const getTruthiness = (value) => (value ? "Truthy!" : "Falsy");
 
-console.log(getTruthiness(getTruthiness(undefined)));
+console.log(getTruthiness(null));
+
+console.log(getTruthiness(getTruthiness(0)));
+
+console.log(getTruthiness("falsy"));
