e0ffbf046ae5c515a93c46b362b1577d4450b913
Ekrem <ekremteke.uk@gmail.com>
2022-09-17 11:47:20 +0100

    truthy and falsy

--- JS1W3exercises/exercise2.js
+++ JS1W3exercises/exercise2.js
@@ -1,21 +1,41 @@
 // console.log(null == undefined);
 
 console.log(1);
-chekIsTrue(1);
+checkIsTrue(1);
+
+console.log(-1);
+checkIsTrue(-1);
 
 console.log(true);
-chekIsTrue(true);
+checkIsTrue(true);
+
+console.log(false);
+checkIsTrue(false);
 
 console.log(0);
-chekIsTrue(0);
+checkIsTrue(0);
+
+console.log(null);
+checkIsTrue(null);
+
+console.log(NaN);
+checkIsTrue(NaN);
 
 //All values are truthy unless they are defined as falsy.
 //That is, all values are truthy except false, 0, -0, 0n, "", null, undefined, and NaN.
 
-function chekIsTrue(value) {
-  if (value) {
-    console.log("♥");
+function checkIsTrue(value1) {
+  if (value1) {
+    // "\x1b[32m%s\x1b[0m" will change console output to green
+    console.log(
+      "\x1b[32m%s\x1b[0m",
+      "Yes, this expression evaluates to true! ✅"
+    );
   } else {
-    console.log(false);
+    // "\x1b[31m%s\x1b[0m" will change console output to red
+    console.log(
+      "\x1b[31m%s\x1b[0m",
+      "Sorry, this expression evaluates to false! ❌"
+    );
   }
 }
