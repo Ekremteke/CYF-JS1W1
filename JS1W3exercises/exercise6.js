ecd093469d87d727b54a242da49e724953de5b5d
Ekrem <ekremteke.uk@gmail.com>
2022-09-17 16:00:11 +0100

    exercises 6 with for loop was done

---
+++ JS1W3exercises/exercise6.js
@@ -0,0 +1,12 @@
+function getIndex(array, item) {
+  for (let i = 0; i < array.length; i++) {
+    if (item === array[i]) {
+      return i;
+    }
+  }
+  return number;
+}
+const arr = [1, 3, 5, 7, 9, 100];
+console.log(getIndex(arr, 5)); // 2
+console.log(getIndex(arr, 100)); // 5
+console.log(getIndex(arr, 1)); // 0
