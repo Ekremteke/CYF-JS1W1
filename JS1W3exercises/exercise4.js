6ab67af988da3dc719417c22459551a870cc668b
Ekrem <ekremteke.uk@gmail.com>
2022-09-17 15:16:49 +0100

    I completed exercise 4 and 5 from lesson

--- JS1W3exercises/exercise4.js
+++ JS1W3exercises/exercise4.js
@@ -6,3 +6,12 @@ console.log(fruits[0]);
 fruits[1] = "raspberry";
 fruits[4] = "pineapple";
 console.log(fruits);
+
+function secondMatchesAmy(array) {
+  for (let i = 0; i <= array.length; i++) {
+    if (array[i] === "Amy") {
+      return "Second element matched!";
+    }
+    return "Second element not matched";
+  }
+}
---
+++ JS1W3exercises/exercise5.js
@@ -0,0 +1,25 @@
+const daysOfWeek = [
+  "Monday",
+  "Tuesday",
+  "Wednesday",
+  "Thursday",
+  "Friday",
+  "Saturday",
+  "Sunday",
+];
+function getDay(index) {
+  if (index < 5) {
+    return `${daysOfWeek[index]} is a business day`;
+  } else if (index < 7) {
+    return `${daysOfWeek[index]} is a weekend day`;
+  } else {
+    return "Uh-oh, the week comprises of just 7 days";
+  }
+}
+
+for (let i = 0; i < daysOfWeek.length; i++) {
+  const dayMessage = "day is: " + daysOfWeek[i];
+  const indexMessage = "index is: " + i;
+  console.log(indexMessage, dayMessage);
+}
+console.log(getDay(5));
